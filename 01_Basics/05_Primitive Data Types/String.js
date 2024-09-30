30043fa0de13e2f316a19db3f2cce86715ac6008
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-09-30 19:14:45 +0500

    String Methods

---
+++ 01_Basics/05_Primitive Data Types/String.js
@@ -0,0 +1,48 @@
+/* String:-
+    String are store the some text and written in the quotes and backticks.
+*/
+
+// Sigle Quotes
+{
+    let text = 'Ahmad Umar'
+    console.log(text);
+}
+
+// double Quotes
+{
+    let text = "Abdul Rasheed"
+    console.log(text)
+}
+
+// Backticks
+{
+    let text = `I am a software engineer.`
+    console.log(text)
+}
+
+
+/*
+
+String Methods:
+1)  String length
+2)  String charAt()
+3)  String charCodeAt()
+4)  String at()
+5)  String [ ]
+6)  String slice()
+7)  String substring()
+8)  String substr()
+9)  String toUpperCase()
+10) String toLowerCase()
+11) String concat()
+12) String trim()
+13) String trimStart()
+14) String trimEnd()
+15) String padStart()
+16) String padEnd()
+17) String repeat()
+18) String replace()
+19) String replaceAll()
+20) String split()
+
+*/
