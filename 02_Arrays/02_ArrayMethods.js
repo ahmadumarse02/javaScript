59d4e746eb4228cd759911b4c6a51f0adb039807
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-10-06 09:40:15 +0500

    length property

--- 02_Arrays/02_ArrayMethods.js
+++ 02_Arrays/02_ArrayMethods.js
@@ -1,22 +1,9 @@
-// Adding Arrary Elements
-
-// Modifying an Array
-{
-  //  push
-
-  let fruits1 = ["Apple", "Banana", "Mango", "Grappes"];
-  console.log(fruits1.push("Orange"));
-  console.log(fruits1);
-  fruits1.push("Pineapple", "Strawberry");
-  console.log(fruits1);
-}
+//  Array Methods:-
 
 {
-  // unshift(): This method add one or more elements begining of an array and return the new lenght of array.
+  //  1) letgth(): this methods is used to return the length of the Array.
 
-  let fruits1 = ["Apple", "Banana", "Mango", "Grappes"];
-  console.log(fruits1.unshift("Orange"));
-  console.log(fruits1);
-  fruits1.unshift("Pineapple", "Strawberry");
-  console.log(fruits1);
+  let fruits = ["Apple", "Banana", "Mango", "Grappes"];
+
+  console.log(fruits.length);
 }
