5530a674b20fa02660873f2c02505d737ec56590
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-10-06 08:49:10 +0500

    Symbol

---
+++ 01_Basics/05_Primitive Data Types/Symbol.js
@@ -0,0 +1,28 @@
+// Symbol is a built-in-funcation whose constructor returns a symbol called a Symbol value that generated a unique.
+//   Symbol are often used to add a unique property key to an object.
+
+{
+  let id = Symbol("Hello");
+  console.log(id);
+}
+
+
+// when we show the description of the symbol we use description method and data type of Symbol description is string. 
+{
+  {
+    let id = Symbol("Hello");
+    console.log(id.description);
+  }
+}
+
+
+{
+    let age = Symbol();
+    let user = {
+        name: "AHmad Umar",
+        class: "Bs(SE)",
+        [age]: '25'
+    }
+    console.log(user)
+    console.log(user[age])
+}
