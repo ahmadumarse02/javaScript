baf8c4febe41ce2c8664aa2e3affb58a84577605
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-10-06 11:25:23 +0500

    flat() mrthod

--- 02_Arrays/02_ArrayMethods.js
+++ 02_Arrays/02_ArrayMethods.js
@@ -108,3 +108,13 @@
 
   console.log(fruits1.copyWithin(2 , 0 , 3))
 }
+
+{
+  // 12) flat(): The flat() method in JavaScript is used to flatten nested arrays. 
+  //     It creates a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+  
+  const myArr = [[1,2],[3,4],[5,6]];
+  const newArr = myArr.flat();
+  console.log(newArr);
+  console.log(myArr); // it does not change the original array.
+  }
