03064b49bd3045f58867260e5aefd2f2faba6655
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-10-07 21:04:39 +0500

    lastIndexOf() Method

--- 02_Arrays/03_ArraySearch.js
+++ 02_Arrays/03_ArraySearch.js
@@ -14,14 +14,26 @@ Array Search Methods:-
 */
 
 // 1) Array indexOf():-
-    // The indexOf() method searches an array for an elements value and return its position.If the value is not found , it return the -1.
+// The indexOf() method searches an array for an elements value and return its position.If the value is not found , it return the -1.
 // Syntax:   array.indexOf(item, start)
 
 {
-    const fruits = ["apple", "banana", "orange" , "apple"];
-    console.log(fruits.indexOf('apple'))
+  const fruits = ["apple", "banana", "orange", "apple"];
+  console.log(fruits.indexOf("apple"));
 
-    console.log(fruits.indexOf('Grapes')) // it return the -1 value because the value is not found in the array.
+  console.log(fruits.indexOf("Grapes")); // it return the -1 value because the value is not found in the array.
 
-    console.log(fruits.indexOf('apple' , 1)) // it return the 3 index.
+  console.log(fruits.indexOf("apple", 1)); // it return the 3 index.
+}
+
+// 2) Array lastIndexOf():-
+// It work similar to the above but the difference is that it return the position of the last occurrence of a specified elements.
+
+{
+  const fruits = ["apple", "banana", "orange", "apple"];
+  console.log(fruits.lastIndexOf("apple")); // it return the 3 index.
+
+  console.log(fruits.lastIndexOf("Grapes")); // it return the -1 value because the value is not found in the array.
+
+  console.log(fruits.lastIndexOf("apple", 1)); // it return the 0index.
 }
