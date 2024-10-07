8932242e6768d1ce086e921b1ccd0c1a45606a8e
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-10-07 23:05:48 +0500

    Adding New Properties

---
+++ 03_object/02_properties.js
@@ -0,0 +1,15 @@
+// Adding New Properties
+
+{
+  const person = {
+    firstname: "John",
+    lastname: "Doe",
+    age: 50,
+    eyecolor: "blue",
+  };
+
+  person.nationality = "English";
+
+  console.log(person)
+}
+
