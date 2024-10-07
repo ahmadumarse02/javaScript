06759f3fc4a572e4edb8461856e1b455c9b7f338
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-10-07 23:06:53 +0500

    Delete Properties

--- 03_object/02_properties.js
+++ 03_object/02_properties.js
@@ -13,3 +13,21 @@
   console.log(person)
 }
 
+// Delete Properties
+
+{
+    const person = {
+      firstname: "John",
+      lastname: "Doe",
+      age: 50,
+      eyecolor: "blue",
+    };
+  
+    person.nationality = "English";
+  
+    delete person.age;
+
+    console.log(person);
+    
+  }
+  
