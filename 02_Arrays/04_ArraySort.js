867d1a832299901931d1b5d9e1a9f587b5165fff
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-10-07 21:32:58 +0500

    Array sort() Method

--- 02_Arrays/04_ArraySort.js
+++ 02_Arrays/04_ArraySort.js
@@ -1,11 +1,25 @@
+/*
+
 Array Search Methods:-
-    1)
-    2)
-    3)
-    4)
-    5)
-    6)
-    7)
-    8)
-    9)
-    10)
+    1) Array sort()
+    2) Array reverse()
+    3) Array toSorted()
+    4) Array toReversed()
+    5) Sorting Objects
+    6) Numeric Sort
+    7) Random Sort
+    8) Math.min()
+    9) Math.max()
+    10) Home made Min()
+    11) Home made Max()
+
+*/
+
+// 1) Array sort();-
+// The sort() method sorts an array alphabetically.
+
+{
+    const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+    console.log(fruits.sort())
+}
