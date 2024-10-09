6b0d67c90b10080404d4a2cc3f020708671c7935
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-10-09 13:18:04 +0500

    Funcation

---
+++ 04_Funcation/01_Funcation.js
@@ -0,0 +1,20 @@
+/*
+
+Funcation:-
+    funcation is a block of that performs a perticular tasks.
+    A funcation is executed when something invoke it(calls it).
+    A funcation is defined with funcation keyword , followed by a name , followed by praentheses().
+
+Syntax:-
+    funcation name() {
+        code to be executed 
+    }
+*/
+
+let x = myFunaction(4 , 3)
+
+function myFunaction(a , b){
+    return a * b;
+}
+
+console.log(x)
