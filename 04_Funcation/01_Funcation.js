796c91e5783522854a129ae805f04f375766f16c
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-10-10 18:11:19 +0500

    Function

--- 04_Funcation/01_Funcation.js
+++ 04_Funcation/01_Funcation.js
@@ -11,10 +11,36 @@ Syntax:-
     }
 */
 
-let x = myFunaction(4 , 3)
+let x = myFunaction(4, 3); // we pass the arguments.
 
-function myFunaction(a , b){
-    return a * b;
+function myFunaction(a, b) {
+  //we pass the parameters.
+  return a * b;
 }
 
-console.log(x)
+console.log(x);
+
+//Objects
+const user = {
+    userName: "Ahmad",
+    rollNo: 232654,
+    class: "BS(SE)",
+}
+
+function userInfo(userObj){
+    console.log(`UserName is ${userObj.userName} and rollNumber is ${userObj.rollNo} and class name is ${userObj.class}`)
+}
+
+userInfo(user)
+
+//Array
+
+const myArray = [200 , 300 , 400 , 500]
+
+function myFunction(getElements){
+    return getElements[2]
+}
+
+console.log(myFunction(myArray))
+
+                  
