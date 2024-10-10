c130c759b0de591ea7e18ec3087770b4f06cf0d3
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-10-11 01:04:44 +0500

    for each loop

---
+++ 05_Iteration/05_foreach.js
@@ -0,0 +1,43 @@
+// for each loop
+
+const array = ["javaScript", "c++", "ruby", "python"];
+
+//using simple function
+// array.forEach(function (val) {
+//   console.log(val);
+// });
+
+// array.forEach((val) => (console.log(val)));
+
+// array.forEach((val) => console.log(val));
+
+array.forEach((val) => {
+  //console.log(val);
+});
+
+function copy(item, index, arr) {
+  console.log(item, index, arr);
+}
+
+array.forEach(copy);
+
+// Array and objects
+
+const coding = [
+  {
+    languageName: "javascript",
+    languageFileName: "js",
+  },
+  {
+    languageName: "java",
+    languageFileName: "java",
+  },
+  {
+    languageName: "python",
+    languageFileName: "py",
+  },
+];
+
+coding.forEach((item) => {
+    console.log(item.languageName)
+})
