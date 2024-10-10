05a35b9bd48f04de522d79913343b3cd48826f8d
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-10-10 22:30:04 +0500

    switch statement

---
+++ 06_ConditionalStatements/02_switch.js
@@ -0,0 +1,34 @@
+// Syntax
+
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 4;
+
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("February");
+    break;
+  case 3:
+    console.log("march");
+    break;
+  case 4:
+    console.log("april");
+    break;
+  case 5:
+    console.log("may");
+    break;
+
+  default:
+    console.log("Invalid Input")
+    break;
+}
