316ebff90fb9632e7ae9392eebf8a0e41ce2bf57
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-10-14 22:07:18 +0500

    array intro

---
+++ 02_dataTypes/non-primitive/arrays/array.js
@@ -0,0 +1,22 @@
+/*
+
+Array:
+Array are special variable that hold more than one value at a time.
+Storing lists of data.
+They can hold any type of value , including other arrays.
+
+*/
+
+{
+  //  Creating an Array
+
+  let fruits1 = ["Apple", "Banana", "Mango", "Grappes"];
+
+  let fruits2 = new Array("Apple", "Banana", "Mango", "Grappes");
+}
+
+{
+  // Accessing Array Elements
+  let fruits1 = ["Apple", "Banana", "Mango", "Grappes"];
+  console.log(fruits1[1]);
+}
