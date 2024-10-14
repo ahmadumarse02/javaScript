b6455db966ad32b7cb2de36e24cb4d79f49a38fc
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-10-14 21:44:07 +0500

    Null Data Tyrp

---
+++ 02_dataTypes/primitive/Null.js
@@ -0,0 +1,9 @@
+// Null value indicates an empty values. the typeof Null is objects.
+
+const var1 = null;
+if (var1) {
+  console.log("var1 is not null");
+} else {
+  console.log("var1 is null");
+}
+console.log(typeof var1);
