b842626346db77bc9a6a1ec8476802227eeee9f2
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-10-15 20:30:01 +0500

    For in Loop

---
+++ 04_iterations/04_forin.js
@@ -0,0 +1,21 @@
+//for in loop
+
+//objects
+
+const lang = {
+  js: "javaScript",
+  cpp: "c++",
+  rb: "ruby",
+};
+
+for (const key in lang) {
+  console.log(lang[key]);
+}
+
+//array
+
+const array = ["javaScript", "c++", "ruby"];
+
+for (const key in array) {
+  console.log(array[key]);
+}
