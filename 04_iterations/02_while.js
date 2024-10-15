764205faee6629dfe6bf72e0bae215bdfdc07e3d
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-10-15 20:29:39 +0500

    While Loop

---
+++ 04_iterations/02_while.js
@@ -0,0 +1,36 @@
+// simple while loop
+
+let i = 0;
+
+while (i <= 10) {
+  console.log(i);
+  //i = i + 2
+  i++;
+}
+
+// Array
+
+const array = ["ahmad", "umar", "umair", "afzal"];
+
+let arr = 0;
+
+while (arr < array.length) {
+  console.log(array[arr]);
+  arr++;
+}
+
+// do while loop
+
+let score = 1;
+
+do {
+  console.log(score);
+  score++;
+} while (score <= 10); // output is 1 to 10
+
+let score1 = 11;
+
+do {
+  console.log(score1);
+  score1++;
+} while (score1 <= 10); // output is 11 it is executed one time
